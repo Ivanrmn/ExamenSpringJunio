9abfb9f9716f00c214c72666dcacfe710966553c
Unknown <ivanrmnh@gmail.com>
2018-06-13 18:24:15 +0200

    JpaRepository

--- src/main/java/org/mvpigs/cotxox/repo/CarreraRepo.java
+++ src/main/java/org/mvpigs/cotxox/repo/CarreraRepo.java
@@ -1,8 +1,10 @@
 package org.mvpigs.cotxox.repo;
 
+import org.mvpigs.cotxox.domain.Carrera;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarreraRepo extends JpaRepository {
+public interface CarreraRepo extends JpaRepository<Carrera, Long> {
+    Carrera findOne(Carrera id);
 }
--- src/main/java/org/mvpigs/cotxox/repo/ConductorRepo.java
+++ src/main/java/org/mvpigs/cotxox/repo/ConductorRepo.java
@@ -1,8 +1,12 @@
 package org.mvpigs.cotxox.repo;
 
+import org.mvpigs.cotxox.domain.Conductor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ConductorRepo extends JpaRepository {
+public interface ConductorRepo extends JpaRepository<Conductor, String> {
+    List<Conductor> findByOcupado(int i);
 }
