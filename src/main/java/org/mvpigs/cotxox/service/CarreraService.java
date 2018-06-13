3a3f68bea43df8f9518cb9f5a9993f5fced34606
Unknown <ivanrmnh@gmail.com>
2018-06-13 18:24:46 +0200

    Interfaz de los servicios

--- src/main/java/org/mvpigs/cotxox/service/CarreraService.java
+++ src/main/java/org/mvpigs/cotxox/service/CarreraService.java
@@ -1,4 +1,18 @@
 package org.mvpigs.cotxox.service;
 
+import org.mvpigs.cotxox.domain.Carrera;
+import org.mvpigs.cotxox.domain.Conductor;
+
 public interface CarreraService {
+    Carrera getConductor(Conductor conductor);
+
+    Carrera getTarjetaCredito();
+
+    Carrera findOne(Long id);
+
+    Long crearCarrera(String s, String parc_de_ses_estacions, String festival_park, int i, int i1);
+
+    Carrera recuperaCarrera(Long idCarrera);
+
+    Carrera updateCarrera(Carrera id);
 }
--- src/main/java/org/mvpigs/cotxox/service/ConductorService.java
+++ src/main/java/org/mvpigs/cotxox/service/ConductorService.java
@@ -1,4 +1,15 @@
 package org.mvpigs.cotxox.service;
 
+import org.mvpigs.cotxox.domain.Conductor;
+
 public interface ConductorService {
+    String getModelo(String modelo);
+
+    String getMatricula(String matricula);
+
+    Conductor getTarjeta(String tarjeta);
+
+    Conductor recuperarConductor(String nombre);
+
+    Conductor recuperarConductorLibre();
 }
