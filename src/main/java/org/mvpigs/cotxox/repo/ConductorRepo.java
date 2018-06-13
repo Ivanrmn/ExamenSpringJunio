a413b402721449b4d6161c4264fdeeded965fe54
Unknown <ivanrmnh@gmail.com>
2018-06-13 16:23:31 +0200

    Repositorios creados

---
+++ src/main/java/org/mvpigs/cotxox/repo/ConductorRepo.java
@@ -0,0 +1,8 @@
+package org.mvpigs.cotxox.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConductorRepo extends JpaRepository {
+}
