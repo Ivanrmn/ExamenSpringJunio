9f814a5443bd736b6b7eff75c23654a6fb136686
Unknown <ivanrmnh@gmail.com>
2018-06-13 16:23:47 +0200

    Servicios creados

---
+++ src/main/java/org/mvpigs/cotxox/service/CarreraService.java
@@ -0,0 +1,4 @@
+package org.mvpigs.cotxox.service;
+
+public interface CarreraService {
+}
---
+++ src/main/java/org/mvpigs/cotxox/service/ConductorServiceImpl.java
@@ -0,0 +1,7 @@
+package org.mvpigs.cotxox.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConductorServiceImpl implements ConductorService {
+}
