f50c0d41a3b60240bf2414e3f785915bad497d4f
Unknown <ivanrmnh@gmail.com>
2018-06-13 18:25:26 +0200

    Servicios implementados

--- src/main/java/org/mvpigs/cotxox/service/CarreraServiceImpl.java
+++ src/main/java/org/mvpigs/cotxox/service/CarreraServiceImpl.java
@@ -1,7 +1,81 @@
 package org.mvpigs.cotxox.service;
 
+import org.mvpigs.cotxox.domain.Carrera;
+import org.mvpigs.cotxox.domain.Conductor;
+import org.mvpigs.cotxox.repo.CarreraRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CarreraServiceImpl implements CarreraService {
+
+    private final CarreraRepo carreraRepo;
+    private final Carrera carrera;
+
+    @Autowired
+    public CarreraServiceImpl(CarreraRepo carreraRepo, Carrera carrera) {
+        this.carreraRepo = carreraRepo;
+        this.carrera = carrera;
+    }
+
+    @Override
+    public Carrera getConductor(Conductor conductor) {
+        if (carrera.getConductor() != null) {
+            return getConductor(conductor);
+        }
+        return null;
+    }
+
+    @Override
+    public Carrera getTarjetaCredito() {
+        if (carrera.getTarjetaCredito() != null) {
+            return getTarjetaCredito();
+        }
+        return null;
+    }
+
+    @Override
+    public Carrera findOne(Long id) {
+        Carrera carrera = carreraRepo.findOne(id);
+        if (carrera != null) {
+            return carrera;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Long crearCarrera(String tarjetaCredito, String origen, String destino, int tiempoEsperado, int tiempoCarrera) {
+        Carrera result = new Carrera();
+        result.getOrigen();
+        result.getDestino();
+        result.getTarjetaCredito();
+        result.getTiempoEsperado();
+        result.getTiempoCarrera();
+        result.getId();
+        result.getDistancia();
+        result.getCosteTotal();
+
+        return Long.valueOf(String.valueOf(result));
+    }
+
+    @Override
+    public Carrera recuperaCarrera(Long id) {
+        Carrera carrera = carreraRepo.findOne(id);
+        String result = carrera.getConductor().getNombre();
+        if (carrera != null) {
+            return carrera;
+        }
+        return null;
+    }
+
+    @Override
+    public Carrera updateCarrera(Carrera id) {
+        Carrera carrera = carreraRepo.findOne(id);
+        if (carrera != null) {
+            return carreraRepo.save(carrera);
+        }
+        return null;
+    }
 }
--- src/main/java/org/mvpigs/cotxox/service/ConductorServiceImpl.java
+++ src/main/java/org/mvpigs/cotxox/service/ConductorServiceImpl.java
@@ -1,7 +1,59 @@
 package org.mvpigs.cotxox.service;
 
+import org.mvpigs.cotxox.domain.Conductor;
+import org.mvpigs.cotxox.repo.ConductorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConductorServiceImpl implements ConductorService {
+
+    private final ConductorRepo conductorRepo;
+    private final Conductor conductor;
+
+    @Autowired
+    public ConductorServiceImpl(ConductorRepo conductorRepo, Conductor conductor) {
+        this.conductorRepo = conductorRepo;
+        this.conductor = conductor;
+    }
+
+    @Override
+    public String getModelo(String modelo) {
+        if(conductor.getModelo() != null){
+            return conductor.getModelo();
+        }
+        return null;
+    }
+
+    @Override
+    public String getMatricula(String matricula) {
+        if(conductor.getMatricula() != null){
+            return conductor.getMatricula();
+        }
+        return null;
+    }
+
+    @Override
+    public Conductor getTarjeta(String tarjeta) {
+        return this.getTarjeta(tarjeta);
+    }
+
+    @Override
+    public Conductor recuperarConductor(String nombre) {
+        Conductor result = new Conductor();
+        result.getNombre();
+        if(result != null) {
+            return result;
+        }
+        return null;
+    }
+
+    @Override
+    public Conductor recuperarConductorLibre() {
+        if(conductor.isOcupado()){
+            return null;
+        } else {
+            return conductor;
+        }
+    }
 }
